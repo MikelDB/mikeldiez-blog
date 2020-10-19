4bf2a2c2e0b5452a62be6d76e8b1312c60beb80e
Mikel Diez Buil <mikel.buil@veygo.com>
2020-10-19 21:56:43 +0100

    Add typescript

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -31,7 +31,6 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-react-svg',
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
